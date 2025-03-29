e6db52e63f046c656ad2d5a21645ef9f8ea97748
R CHARANTEJ REDDY <129316635+charantek1styearbtech@users.noreply.github.com>
2025-03-29 15:14:30 +0530

    Update updatePostmanCollection.js
    - reorderd the line

--- postman/scripts/updatePostmanCollection.js
+++ postman/scripts/updatePostmanCollection.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const axios = require("axios");
 
 const POSTMAN_COLLECTION_PATH = "../collections/Gemini API.postman_collection.json";
-const GEMINI_ENDPOINTS_URL = `https://generativelanguage.googleapis.com/$discovery/OPENAPI3_0?version=v1beta&key=${api_key}`;
 const POSTMAN_API_KEY = process.env.POSTMAN_API_KEY;
 const COLLECTION_UID = process.env.COLLECTION_UID;
-const api_key = process.env.GEMINI_API_KEY;
+const api_key = process.env.API_KEY;
+const GEMINI_ENDPOINTS_URL = `https://generativelanguage.googleapis.com/$discovery/OPENAPI3_0?version=v1beta&key=${api_key}`;
 
 if (!POSTMAN_API_KEY || !COLLECTION_UID) {
   console.error("Please set the POSTMAN_API_KEY and COLLECTION_UID environment variables.");
